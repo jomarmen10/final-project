b2adb6027edd200476f576374deb4281c6e78f25
Jomar Mendoza <jmm21@Jomars-MacBook-Pro.local>
2019-06-10 09:53:04 -0700

    some test

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const corsOptions = {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors(corsOptions));
-
+app.use(express.static(`${___dirname}/build`))
 
 const server = http.Server(app);
 
@@ -55,12 +55,15 @@ io.on('connection', (socket) => {
 })
 
 
-if(process.env.NODE_ENV === 'production'){
-  app.get('/*', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-  })
-}
+// if(process.env.NODE_ENV === 'production'){
+//   app.get('/*', (req, res) =>{
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'))
+//   })
+// }
 
+app.get('/*', (req, res) =>{
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.get('/', async(req, res) =>{
   try {
